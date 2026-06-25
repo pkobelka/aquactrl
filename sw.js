ac827ee0f386ea15b78569d45b6282876f0619cf
pkobelka <p.kobelka@gmail.com>
2026-06-25 11:53:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // AquaControl – samostatný service worker pro /aquacontrol/
 // Účel: oddělit appku od "budek" (vlastní scope) a umožnit instalaci na plochu.
-const CACHE = 'aqua-v4';
+const CACHE = 'aqua-v6';
 const ASSETS = [
   './',
   './index.html',
